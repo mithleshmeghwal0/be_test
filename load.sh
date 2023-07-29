2d4f7f2188da64b30f39df79b00f402011c844f3
Mithlesh Meghwal <115926756+mithleshmeghwal0@users.noreply.github.com>
2023-07-29 12:52:08 +0700

    Update load.sh

--- load.sh
+++ load.sh
@@ -5,5 +5,5 @@ SERVER_URL="http://localhost:8080/api/user/v1/users"
 ACCESS_TOKEN="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJEYXRhIjoiN2Q0MTI2MjgtMGJmYi00NTdjLWFiYjgtMTFmZjQ5MGZlMDM0IiwiaXNzIjoidGVzdCIsInN1YiI6InNvbWVib2R5IiwiZXhwIjoxNjkzMTAyNTA5LCJuYmYiOjE2OTA1MTA1MTYsImlhdCI6MTY5MDUxMDUxNiwianRpIjoiZDk0ZDYxNDAtMzc0My00MmJmLThkODQtMGQ0ZDcwNTQ0OTVlIn0.pd4Z1S8wsQQCDf2b-uIgLw3azb4thY0RCH7-FCdzp_U"
 
 # Perform load testing with ab
-ab -k -n 1000 -c 100 -p testdata.txt -T application/json -H "Accept-Encoding: gzip, deflate" -H "Authorization: Bearer ${ACCESS_TOKEN}" "${SERVER_URL}"
+ab -k -n 10000 -c 100 -p testdata.txt -T application/json -H "Accept-Encoding: gzip, deflate" -H "Authorization: Bearer ${ACCESS_TOKEN}" "${SERVER_URL}"
 
